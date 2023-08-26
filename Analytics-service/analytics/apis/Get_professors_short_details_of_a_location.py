e8669454711384ca7f996c48f8234866664530ac
ANIK115 <aniknj4@gmail.com>
2023-08-26 12:44:14 +0600

    changed api of get all professors details

--- Analytics-service/analytics/apis/Get_professors_short_details_of_a_location.py
+++ Analytics-service/analytics/apis/Get_professors_short_details_of_a_location.py
@@ -10,11 +10,11 @@ from analytics.models.location import *
 class Get_professor_Short_Details_of_a_Location(Resource):
     @api.doc(responses={200: 'OK', 404: 'Not Found', 500: 'Internal Server Error'})
 
-    def post(self,location_id):
+    def get(self,location_id):
 
         try:
 
-            response = requests.get(f'http://localhost:5002/api/professors/{location_id}/get_professors_short_details')
+            response = requests.get(f'http://localhost:5002/api/professors/{location_id}/get_location_based_professors')
             return response.json()
 
         except Exception as e:
